0b2b42de05045cfd3d722a390d40ce02186090f2
Shakhzod235 <shakhzodikramov235@gmail.com>
2024-02-27 18:57:38 +0500

    feat: add audio list and implement pressbtn

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,26 @@
       <div class="wrapper">
         <div class="simon">
           <div class="simon-board">
-            <div class="red simon-card"></div>
-            <div class="blue simon-card"></div>
-            <div class="yellow simon-card"></div>
-            <div class="green simon-card"></div>
+            <div
+              class="red simon-card"
+              ref="1"
+              @click="pressBtn(1)"
+            ></div>
+            <div
+              class="blue simon-card"
+              ref="2"
+              @click="pressBtn(2)"
+            ></div>
+            <div
+              class="yellow simon-card"
+              ref="3"
+              @click="pressBtn(3)"
+            ></div>
+            <div
+              class="green simon-card"
+              ref="4"
+              @click="pressBtn(4)"
+            ></div>
           </div>
           <button class="simon-btn">
             {{ buttonText }}
@@ -44,9 +60,45 @@
         level: "Легкий",
         levels: ["Легкий", "Средний", "Сложный"],
         buttonText: "Играть",
+        levelTime: {
+          Легкий: "1500",
+          Средний: "1000",
+          Сложный: "400",
+        },
+        audio: {
+          1: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+          2: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+          3: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+          4: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
+        },
+        orderList: [],
+        userList: [],
+        timeoutID: null,
       };
     },
-    methods: {},
+    methods: {
+      pressBtn(id) {
+        this.audio[id].play();
+        this.$refs[id].style.opacity = "1";
+        setTimeout(() => {
+          this.$refs[id].style.opacity = "0.5";
+        }, 100);
+        if (this.inGame) {
+          clearTimeout(this.timeoutID);
+          this.userList.push(id);
+        }
+      },
+      start() {
+        this.orderList = [];
+        this.userList = [];
+        this.time = this.levelTime[this.level];
+      },
+
+      step() {
+        this.userList = [];
+        this.buttonText = "Слушайте";
+      },
+    },
   };
 </script>
 
